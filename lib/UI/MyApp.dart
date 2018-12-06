8d491498a609700339b567826571209237f27bdd
Yuji Imamura <YI201610@users.noreply.github.com>
2018-12-07 00:24:07 +0900

    リファクタリング

--- lib/UI/MyApp.dart
+++ lib/UI/MyApp.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:FlutterTips/Domain/iTunes.dart';
 
+import 'package:FlutterTips/UI/MyHomePage.dart';
+
+/*  */
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -17,53 +20,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  final String title;
-
-  /* Constructor */
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-
-
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'ここに文言を設定',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ),
-    );
-  }
-}
