358ceb6a383cf4e45321f3eba6effa8d8b6cbced
Yuji Imamura <YI201610@users.noreply.github.com>
2018-12-13 00:02:46 +0900

    コメントらを追加

--- lib/UI/Menu/Building-a-layout-Tutorial/BuildingALayoutTutorial.dart
+++ lib/UI/Menu/Building-a-layout-Tutorial/BuildingALayoutTutorial.dart
@@ -1,59 +1,87 @@
 import 'package:flutter/material.dart';
 
+/* Building Layout公式サンプル*/
 class BuildingALayoutTutorial extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
 
+    /*タイトルセクションのウィジェット*/
     Widget titleSection = Container(
       padding: const EdgeInsets.all(32.0),
+
+      /* 行 */
       child: Row(
         children: [
+
+          /*伸びる領域*/
           Expanded(
+
+            /*列*/
             child: Column(
+              /*寄せ方*/
               crossAxisAlignment: CrossAxisAlignment.start,
+
               children: [
+
                 Container(
-                  padding: const EdgeInsets.only(bottom: 8.0),
+                  /*タイトルと、サブテキストの間の隙間ポイント*/
+                  padding: const EdgeInsets.only(bottom: 20.0),
                   child: Text(
-                    'タイトルテキスト',
+                    'タイトルテキストタイトルテキストタイトルテキストタイトルテキストタイトルテキストタイトルテキストタイトルテキストタイトルテキストタイトルテキストタイトルテキスト',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
+
                 Text(
-                  'サブテキスト',
+                  'サブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキストサブテキスト',
                   style: TextStyle(
                     color: Colors.grey[500],
                   ),
                 ),
+
               ],
             ),
           ),
+
           Icon(
-            Icons.star,
+            Icons.stars,
             color: Colors.red[500],
           ),
-          Text('41'),
+
+          Text('末尾のテキスト'),
         ],
       ),
-    );
+    ); /* titleSection */
 
+
+    /*ボタンを含む列を生成する(nested function)*/
     Column buildButtonColumn(IconData icon, String label) {
+
       Color color = Theme.of(context).primaryColorDark;
 
+      /*列*/
       return Column(
         mainAxisSize: MainAxisSize.min,
+        //mainAxisSize: MainAxisSize.max,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+
+          /*アイコンを追加*/
           Icon(icon, color: color),
+
+          /*隙間とテキストを含むコンテナを追加*/
           Container(
-            margin: const EdgeInsets.only(top: 8.0),
+            /* アイコンとテキストの間のポイント数*/
+            margin: const EdgeInsets.only(top: 20.0),
+
             child: Text(
               label,
               style: TextStyle(
                 fontSize: 12.0,
-                fontWeight: FontWeight.w400,
+                fontWeight: FontWeight.w700,  //Bold的な属性: w100〜w900
                 color: color,
               ),
             ),
@@ -62,28 +90,46 @@ class BuildingALayoutTutorial extends StatelessWidget {
       );
     }
 
+    /*ボタンセクション*/
     Widget buttonSection = Container(
+      /*行*/
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        /*均等配置*/
+        //mainAxisAlignment: MainAxisAlignment.start, //
+        //mainAxisAlignment: MainAxisAlignment.center, //
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly, //均等配置
+        //mainAxisAlignment: MainAxisAlignment.spaceAround, //幅ぴったり
         children: [
-          buildButtonColumn(Icons.call, '電話'),
-          buildButtonColumn(Icons.near_me, '位置情報'),
-          buildButtonColumn(Icons.share, '共有'),
+          /*3つのボタン列を追加する*/
+          buildButtonColumn(Icons.call, '電話電話電話'),
+          buildButtonColumn(Icons.near_me, '位置情報位置情報'),
+          buildButtonColumn(Icons.share, '共有共有共有'),
         ],
       ),
     );
 
+    /*テキストセクション*/
     Widget textSection = Container(
       padding: const EdgeInsets.all(32.0),
       child: Text(
         '''
-複数行のテキスト。行1。       
+複数行のテキスト。行1。複数行のテキスト。行1。複数行のテキスト。行1。複数行のテキスト。行1。複数行のテキスト。行1。複数行のテキスト。行1。       
 複数行のテキスト。行2。       
 複数行のテキスト。行3。       
 複数行のテキスト。行4。       
 複数行のテキスト。行5。       
         ''',
-        softWrap: true,
+        softWrap: true,  //領域内で改行するかのフラグ
+      ),
+    );
+
+    Widget imageSection = Container (
+      padding: const EdgeInsets.all(5.0),
+      child: Image.asset(
+        'images/sandwich.jpg',
+        width: 600.0,
+        height: 240.0,
+        fit: BoxFit.cover,
       ),
     );
 
@@ -93,14 +139,8 @@ class BuildingALayoutTutorial extends StatelessWidget {
       ),
       body: ListView(
         children: [
-
-          Image.asset(
-            'images/sandwich.jpg',
-            width: 600.0,
-            height: 240.0,
-            fit: BoxFit.cover,
-          ),
-
+          /*これまで定義した、複数のセクションウィジェットを追加*/
+          imageSection,
           titleSection,
           buttonSection,
           textSection,
